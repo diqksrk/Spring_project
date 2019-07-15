36419309b9c2f312476af3169cbb4ef085ac8802
diqksrk <diqksrk123@naver.com>
2019-07-15 13:58:00 +0900

    crud 추가

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -4,13 +4,14 @@ import com.example.demo.service.BoardService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/board")
+@Controller
+@RequestMapping("/title")
 @Log
 public class PageController {
     @Autowired
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,9 +4,9 @@ TITLE VARCHAR2(20),
 CONTENTS LONG,
 CREATION_DATE DATE DEFAULT SYSDATE);
 
-create sequence seq_reply;
+create sequence seq_board;
 
 alter table board_table add constraint pk_test_board primary key(rno);
 
-insert into board_table (RNO, TITLE, CONTENTS) values (seq_reply.nextval, 'goo', 'goooo');
-insert into board_table (RNO, TITLE, CONTENTS) values (seq_reply.nextval, 'boo', 'boooo');
+insert into board_table (RNO, TITLE, CONTENTS) values (seq_board.nextval, 'goo', 'goooo');
+insert into board_table (RNO, TITLE, CONTENTS) values (seq_board.nextval, 'boo', 'boooo');
